f6786572bfaa72f1d8352d6ea6f2edf0c0d0d6a8
brendon.hall@gmail.com <brendon.hall@gmail.com>
2022-12-20 14:33:48 -0600

    add coverage

---
+++ farmfinder/NDVA_extractor.py
@@ -0,0 +1,3 @@
+def ndva_from_values(red, infred):
+
+    return (infred - red) / (red + infred)
--- tests/unit/test_NDVA_extractor.py
+++ tests/unit/test_NDVA_extractor.py
@@ -1,13 +1,15 @@
 # import numpy as np
-# import logging
+import logging
 
 import os
 from pathlib import Path
 
 import pytest
 
+from farmfinder.NDVA_extractor import ndva_from_values
 
-# LOGGER = logging.getLogger(__name__)
+
+LOGGER = logging.getLogger(__name__)
 
 _here = Path(os.path.abspath(os.path.dirname(__file__)))
 _test_data_path = _here / "data"
@@ -21,7 +23,8 @@ def open_test_img():
 
 
 def test_NDVA_extractor():
-    assert True == True
+    a = 1
+    assert a == 1
 
 
 @pytest.mark.parametrize(
@@ -33,3 +36,8 @@ def test_eval(test_input, expected):
 
 def test_img_read(open_test_img):
     img = open_test_img
+    assert isinstance(img, object)
+
+
+def test_ndva_from_values():
+    assert ndva_from_values(1, 3) == 0.5
